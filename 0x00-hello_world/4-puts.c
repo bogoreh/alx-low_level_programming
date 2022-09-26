bde0b80bbfbd03d87f56028e1ca489a3f4f501f3
bogoreh <bogoreh026@gmail.com>
2022-09-26 23:18:17 +0300

    update commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+
 /**
- *  * main - Entry point
- *Description: prints
- *Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
-{ puts("\"Programming is like building a multilingual puzzle");
+{
+puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }
