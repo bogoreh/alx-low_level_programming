29be6db90a10a4ade548a72b00b052b2fe3ad6dd
bogoreh <76124689+bogoreh@users.noreply.github.com>
2022-12-09 19:13:27 +0300

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,3 +1,8 @@
+/*
+ * File: 100-print_comb3.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
 /**
@@ -6,7 +11,6 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	int digit1, digit2;
