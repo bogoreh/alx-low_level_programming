4580ca6f4b4a5940316bbe9fc2fd0c3e054982d7
bogoreh <bogoreh026@gmail.com>
2022-12-14 12:44:43 -0800

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,39 @@
+nclude "main.h"
+
+/**
+ *
+ *  * _islower - just an if statement
+ *
+ *   * @c: is an Ascii character
+ *
+ *    * Return: always 0 bros
+ *
+ *    */
+
+int _islower(int c)
+
+
+
+{
+
+		if (c >= 'a' && c <= 'z')
+
+
+
+		{
+
+					return (1);
+
+
+
+		}
+
+			else
+
+			{
+
+						return (0);
+
+			}
+
+}
