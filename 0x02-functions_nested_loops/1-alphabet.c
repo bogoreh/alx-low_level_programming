79477b0750215b7aea538ceadd4398ab866c48ab
bogoreh <bogoreh026@gmail.com>
2022-12-14 12:33:00 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,37 @@
+nclude "main.h"
+
+/**
+ *
+ * * print_alphabet - print alphabet
+ *
+ * * main - prin
+ *
+ * *Return: empty from the void function
+ *
+ *  */
+
+
+
+void print_alphabet(void)
+
+{
+
+		char alpha = 'a';
+
+
+
+		while (alpha <= 'z')
+
+		{
+
+			_putchar(alpha);
+
+
+
+				alpha++;
+
+		}
+
+		_putchar('\n');
+
+}
