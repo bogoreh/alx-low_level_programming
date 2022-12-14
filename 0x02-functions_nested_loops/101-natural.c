f49330197d50a9862e1873ce0813df588ae67255
bogoreh <76124689+bogoreh@users.noreply.github.com>
2022-12-15 00:53:11 +0300

    Update 101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,39 +1,20 @@
-nclude <stdio.h>
-
+#include <stdio.h>
 /**
- *
- *  * main -finding and printing out the sum tota of the multiples of five
- *
- *   * Return: always returns zero to my satifaction
- *
- *    */
-
+ * main -finding and printing out the sum tota of the multiples of five
+ * Return: always returns zero to my satifaction
+ */
 int main(void)
-
 {
+	int num = 0, sumOf;
 
-		int num = 0, sumOf;
-
-
-
-			while (num < 1024)
-
-					{		num++;
-
-							if (num % 5 == 0 || num % 3 == 0)
-
-									{
-
-												sumOf += num;
-
-													}
-
-					}
-
-			printf("%d", sumOf);
-
-			printf("\n");
-
-			return (0);
-
+	while (num < 1024)
+	{		num++;
+	if (num % 5 == 0 || num % 3 == 0)
+	{
+		sumOf += num;
+	}
+}
+printf("%d", sumOf);
+printf("\n");
+return (0);
 }
