0814e0b30517968d15ad59aa5396abb4936feddb
bogoreh <76124689+bogoreh@users.noreply.github.com>
2022-12-14 23:46:44 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,39 +1,20 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *
- *  * _islower - just an if statement
- *
- *   * @c: is an Ascii character
- *
- *    * Return: always 0 bros
- *
- *    */
-
+ * _islower - just an if statement
+ * @c: is an Ascii character
+ * Return: always 0 bros
+*/
 int _islower(int c)
 
-
+{
+	if (c >= 'a' && c <= 'z')
 
 {
+		return (1);
 
-		if (c >= 'a' && c <= 'z')
-
-
-
-		{
-
-					return (1);
-
-
-
-		}
-
-			else
-
-			{
-
-						return (0);
-
-			}
-
+}
+	else
+{
+		return (0);
+}
 }
