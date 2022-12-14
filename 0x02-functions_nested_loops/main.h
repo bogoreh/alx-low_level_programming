6a1b97e3f1461a1d5d2c8bd4c772b91c27b9725d
bogoreh <76124689+bogoreh@users.noreply.github.com>
2022-12-14 23:12:15 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -27,16 +27,4 @@ void print_to_98(int n);
 
 void print_times_table(int n);
 
-<<<<<<< HEAD
-									void jack_bauer(void);
-
-										void times_table(void);
-
-											int add(int, int);
-
-												void print_to_98(int n);
-
-void print_times_table(int n);
-=======
->>>>>>> af260dcc2f1088c4fb66e3c96409fbaa603dae28
 #endif
