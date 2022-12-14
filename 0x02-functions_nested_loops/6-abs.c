65fbdd1988da3908e23bdc296555d264845e35b0
bogoreh <76124689+bogoreh@users.noreply.github.com>
2022-12-15 00:01:14 +0300

    Update 6-absc

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,57 +1,29 @@
-nclude "main.h"
-
+#include "main.h"
 #include <stdlib.h>
-
 #include <stdio.h>
-
 /**
+ * _abs - computes the value of an integer
+ * @boy: is an integer
+ * main - check the code
  *
- *  * _abs - computes the value of an integer
+ * Return: Always 0.
  *
- *   * @boy: is an integer
  *
- *    * main - check the code
- *
- *     *
- *
- *      * Return: Always 0.
- *
- *       *
- *
- *        *
- *
- *         */
-
+ */
 int _abs(int boy)
-
-
 
 {
 
-
-
-		if (boy > 0)
-
-		{
-
-					return (boy);
-
-						}
-
-			else if (boy < 0)
-
-					{
-
-								return (-boy);
-
-									}
-
-				else
-
-						{
-
-									return (boy);
-
-										}
-
+	if (boy > 0)
+{
+		return (boy);
+	}
+	else if (boy < 0)
+	{
+		return (-boy);
+	}
+	else
+	{
+		return (boy);
+	}
 }
