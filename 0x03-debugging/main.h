de6bf0ef86a6fdf3307b8c8eee6261da86a38995
bogoreh <76124689+bogoreh@users.noreply.github.com>
2022-12-15 21:48:22 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,1 +1,11 @@
-kjfdkjk
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int convert_day(int month, int day);
+int largest_number(int a, int b, int c);
+#endif /* MAIN_H */
