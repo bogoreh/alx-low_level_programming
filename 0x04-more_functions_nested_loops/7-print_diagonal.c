087363515b9eaae75cdb00d6ddedc314df4d55e3
bogoreh <76124689+bogoreh@users.noreply.github.com>
2022-12-15 23:31:51 +0300

    Update 7-diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,38 +1,38 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- * * print_diagonal - draws line
- * * @n: number og\f lines
- * *
- * * Return: empty
- * */
+* print_diagonal - draws line
+* @n: number og\f lines
+*
+* Return: empty
+*/
 
 void print_diagonal(int n)
 
 {
 
-	int line = 0;
-	int spaces;
+int line = 0;
+int spaces;
 
-	if (n > 0)
+if (n > 0)
 
-	{
+{
 
-		while (line < n)
+while (line < n)
 
-		{
+{
 
-			for (spaces = 0; spaces < line; spaces++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-			line++;
-		}
+for (spaces = 0; spaces < line; spaces++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+line++;
+}
+}
+else
+{
+_putchar('\n');
 	}
-	else
-	{
-		_putchar('\n');
-			}
 }
