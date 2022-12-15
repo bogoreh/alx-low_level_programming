d1fb5cc2ff36c08f4f4a7ad2746ba4ccf8e76d12
bogoreh <76124689+bogoreh@users.noreply.github.com>
2022-12-15 23:58:01 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,53 +1,28 @@
-nclude "main.h"
-
-
+#include "holberton.h"
+#include <stdio.h>
 
 /**
- *
- *  * print_number - prints an integer
- *
- *   * @n: integer to be printed
- *
- *    */
-
+  * print_number - Prints an integer.
+  * @n: The integer to prints.
+  *
+  * Return: Nothing!
+  */
 void print_number(int n)
-
 {
+	unsigned int k = n;
 
-	 unsigned int n1;
+	if (n < 0)
+	{
+		n *= -1;
+		k = n;
+		_putchar('-');
+	}
 
+	k /= 10;
 
+	if (k != 0)
+		print_number(k);
 
-	  if (n < 0)
-
-		   {
-
-			    n1 = -n;
-
-			     _putchar('-');
-
-			      } else
-
-				       {
-
-					        n1 = n;
-
-						 }
-
-
-
-			       if (n1 / 10)
-
-				        {
-
-						 print_number(n1 / 10);
-
-						  }
-
-
-
-			        _putchar((n1 % 10) + '0');
+	_putchar((unsigned int) n % 10 + '0');
 
 }
-
-
